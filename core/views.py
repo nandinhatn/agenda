8e5ccc52e35acbe8d275d953d94d2d24c4d2d439
nandatnl <fernanda@poppymidia.com.br>
2022-12-26 08:51:04 -0300

    incluindo funcao tela autenticacao

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect
-
-
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login, logout
+from django.contrib import messages
 
 
 # Create your views here.
@@ -11,6 +12,27 @@ def consulta(request,titulo):
 
     return HttpResponse('<h1> o local é {} data {}</h1>'.format(teste, data))
 
+def login_user(request):
+    return render(request,'login.html')
+
+def submit_login(request):
+    if request.POST:
+        username= request.POST.get('username')
+        password= request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request,usuario)
+            return redirect('/')
+        else:
+            messages.error(request, 'Usuario ou senha inválido')
+
+        return redirect('/')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
     #evento = Evento.objects.get(id=1)
     evento = Evento.objects.all()
@@ -21,3 +43,5 @@ def lista_eventos(request):
 
 #def index(request):
  #   return redirect('agenda/')
+
+
